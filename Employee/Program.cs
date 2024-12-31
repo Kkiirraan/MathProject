0e70546c68f16261f37937523b3af243fc353b61
kiran <kiranvijayanv@gmail.com>
2024-12-31 10:32:03 +0530

    Add project files.

---
+++ Employee/Program.cs
@@ -0,0 +1,16 @@
+﻿using MathLibrary;
+
+namespace MathProject
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int add = MathOperations.Add(1, 2);
+            Console.WriteLine(add);
+
+            int sub=MathOperations.Sub(1, 2);
+            Console.WriteLine(sub);
+        }
+    }
+}
---
+++ MathLibrary/MathOperations.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathLibrary
+{
+    public class MathOperations
+    {
+        public static int Add(int a,int b)
+        {
+            return a + b;
+        }
+        public static int Sub(int a,int b)
+        {
+            return a - b;
+        }
+        public static int Mul(int a,int b)
+        {
+            return a * b;
+        }
+        public static int Div(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
